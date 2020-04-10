6f4ec37ba35daada9db50e73f6c22f71c946f32d
luqmanul612 <luqmanul612@gmail.com>
2020-04-10 18:40:00 +0700

    update setting.py

--- ims/settings.py
+++ ims/settings.py
@@ -139,7 +139,7 @@ THOUSAND_SEPARATOR = "."
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-#STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
