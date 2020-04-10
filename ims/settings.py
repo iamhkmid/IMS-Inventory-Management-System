d380f48fed769fc129fd8096122f30849bf3ee10
luqmanul612 <luqmanul612@gmail.com>
2020-04-10 18:34:14 +0700

    update setting.py

--- ims/settings.py
+++ ims/settings.py
@@ -136,8 +136,8 @@ THOUSAND_SEPARATOR = "."
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
